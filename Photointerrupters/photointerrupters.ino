3eb86d7028ad0e94a853ab2f522c4480f798fa52
lfuller20 <56936178+lfuller20@users.noreply.github.com>
2019-11-18 14:36:38 -0500

    Update photointerrupters.ino

--- Photointerrupters/photointerrupters.ino
+++ Photointerrupters/photointerrupters.ino
@@ -1,13 +1,13 @@
 const byte ledPin = 13;
 const byte interruptPin = 2;
-volatile byte state = LOW;
+volatile byte state = LOW; // defines the state of the photointerruptor
 
 void setup ()
 {
 	Serial.begin(9600);
 	pinMode(ledPin, OUTPUT);
 	pinMode(interruptPin, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
+	attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING); //commands the LED to blink when the photointerruptor pin goes from LOW to HIGh
 }
 
 void loop ()
