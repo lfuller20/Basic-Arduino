90553d149c0debd7028f3aa914d93df7c96253c7
lfuller20 <56936178+lfuller20@users.noreply.github.com>
2019-12-02 21:48:04 -0500

    Update potentiometers.ino

--- Potentiometers/potentiometers.ino
+++ Potentiometers/potentiometers.ino
@@ -9,20 +9,20 @@ int brightness = 0;
 void setup()
 {
 	pinMode(ledPin, OUTPUT);
-	pinMode(potPin, INPUT);
+	pinMode(potPin, INPUT); // sets the potentiometer pin as the input
 	Serial.begin(9600);
 	lcd.init();
 	lcd.backlight();
 	lcd.setCursor(0,0);
-	lcd.print("Brightness:");
+	lcd.print("Brightness:"); // prints brightness
 }
 
 void loop()
 {
 	brightness = analogRead(0);
-	brightness = map(brightness, 0, 1023, 0, 10);
+	brightness = map(brightness, 0, 1023, 0, 10); // maps the brightness of the LED
 	lcd.setCursor(0,1);
-	lcd.print(brightness);
+	lcd.print(brightness); // prints the brightness of the LED at that moment in time.
 	lcd.print("    ");
 	brightness = map(brightness, 0, 10, 0, 255);
 	analogWrite(ledPin, brightness);
