e340b1829fe4fe32a966e96292509870a4a930cd
lfuller20 <56936178+lfuller20@users.noreply.github.com>
2020-01-26 16:43:21 -0500

    Update fade LED.ino

--- fade LED/fade LED.ino
+++ fade LED/fade LED.ino
@@ -1,20 +1,20 @@
 int led = 9;          
 int brightness = 0;  
-int fadeAmount = 5;   
+int fadeAmount = 5; // specifies the variable of the fade amount
 
 void setup() {
   pinMode(led, OUTPUT);
-  Serial.begin(9600); 
+  Serial.begin(9600); // starts the serial monitor
 }
 
 void loop() {
   analogWrite(led, brightness);
 
-  brightness = brightness + fadeAmount;
+  brightness = brightness + fadeAmount; 
 
   if (brightness <= 0 || brightness >= 255) {
     fadeAmount = -fadeAmount;
   }
-  Serial.println(brightness);
+  Serial.println(brightness); // prints the brightness of the led as it is fading in and out
   delay(20);
 }
