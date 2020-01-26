ddeda420a1a84a6b63219bf77c157b49d53b2018
lfuller20 <56936178+lfuller20@users.noreply.github.com>
2020-01-26 16:13:22 -0500

    Update buttonpresses.ino

--- button presses/buttonpresses.ino
+++ button presses/buttonpresses.ino
@@ -1,8 +1,8 @@
 #include <Wire.h> // For I2C
 #include <LiquidCrystal_I2C.h> 
-const int buttonPin = 12;
-int buttonCounter = 0;
-int buttonState = 0; 
+const int buttonPin = 12; // Establishes the button pin
+int buttonCounter = 0; // Establishes the starting value of the button counter
+int buttonState = 0; // Establishes the state of the button prior to being pressed
 int bpressed = 0;
 
 LiquidCrystal_I2C lcd(0x27,16,2); 
@@ -21,7 +21,7 @@ void loop()
    Serial.print(buttonState);
    if ((buttonState == HIGH) && (bpressed == 0))
    {
-   	buttonCounter = buttonCounter + 1;
+   	buttonCounter = buttonCounter + 1; // Increases the value of the button counter by 1.
    	lcd.setCursor(0,1);
    	lcd.print(buttonCounter); 
    	bpressed = 1; 
